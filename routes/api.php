7905b3beecab8ad7f8f563f0d75ed82cb2343667
Md Ariful Islam <imdarif122@gmail.com>
2023-05-20 14:36:04 +0600

    Add github

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::any('/github/webhook/{github_key}', function ($github_key) {
+    if($github_key && $github_key === env('GITHUB_WEBHOOK_KEY'))
+    {
+        dump(shell_exec('git pull'));
+        dump(shell_exec('composer update'));
+        dump(shell_exec('php artisan migrate --force'));
+    }
+    return abort(404);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
